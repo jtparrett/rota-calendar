33902a3dc574f0bbdc6db1bb23ee0b86a3e6fbbd
jtparrett <jtparrett@me.com>
2018-12-23 12:03:22 +0000

    split calendar days into container

--- src/molecules/CalendarDays/index.tsx
+++ src/molecules/CalendarDays/index.tsx
@@ -1,73 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import dayjs from 'dayjs'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {compose} from 'recompose'
-
-// Atoms
-import Day from '../../atoms/Day'
-import Modal from '../../atoms/Modal'
-import Button from '../../atoms/Button'
-import Typography from '../../atoms/Typography'
-
-// Config
-import {daysToShow} from '../../config'
 
 // Context
 import {withCalendar} from '../../context/calendar'
 
-// Hooks
-const today = dayjs().startOf('day')
-
-const useDays = (dayOffset: number) => {
-  
-  const getDays = () => {
-    return [...Array(daysToShow)].map((_, index) => {
-      return today.add(index + dayOffset, 'day')
-    })    
-  }
-
-  const [days, setDays] = useState(getDays)
-
-  useEffect(() => {
-    setDays(getDays)
-  }, [dayOffset])
-
-  return days
-}
-
-// Render
-const CalendarDays = ({ dayOffset }) => {
-  const days: any[] = useDays(dayOffset)
-  const [modalOpen, setModalOpen] = useState(false)
-  const [selectedDate, setSelectedDate] = useState(today)
-
-  return (
-    <React.Fragment>
-      {days.map(date => (
-        <Day 
-          key={date.format('DD/MM/YYYY')} 
-          date={date}
-          addBooking={(date) => {
-            setSelectedDate(date)
-            setModalOpen(true)
-          }} />
-      ))}
-
-      {modalOpen &&
-        <Modal>
-          <Button title="Close" onClick={() => setModalOpen(false)} />
-          <Typography margin="10px 0">{selectedDate.format('MMMM D : HH:mm')}</Typography>
-          <Button title="Book this slot" onClick={() => setModalOpen(false)} />
-        </Modal>
-      }
-    </React.Fragment>
-  )
-}
-
-CalendarDays.propTypes = {
-  dayOffset: PropTypes.number.isRequired
-}
+// View
+import View from './view'
 
 // Redux Queries
 const mapStateToProps = (store, props) => ({
@@ -78,4 +16,4 @@ const mapStateToProps = (store, props) => ({
 export default compose(
   withCalendar,
   connect(mapStateToProps)
-)(CalendarDays)
+)(View)
---
+++ src/molecules/CalendarDays/view.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
+import PropTypes from 'prop-types'
+
+// Atoms
+import Day from '../../atoms/Day'
+import Modal from '../../atoms/Modal'
+import Button from '../../atoms/Button'
+import Typography from '../../atoms/Typography'
+
+// Config
+import {daysToShow} from '../../config'
+
+// Hooks
+const today = dayjs().startOf('day')
+
+const useDays = (dayOffset: number) => {
+  
+  const getDays = () => {
+    return [...Array(daysToShow)].map((_, index) => {
+      return today.add(index + dayOffset, 'day')
+    })    
+  }
+
+  const [days, setDays] = useState(getDays)
+
+  useEffect(() => {
+    setDays(getDays)
+  }, [dayOffset])
+
+  return days
+}
+
+// Render
+const CalendarDays = ({ dayOffset }) => {
+  const days: any[] = useDays(dayOffset)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [selectedDate, setSelectedDate] = useState(today)
+
+  return (
+    <React.Fragment>
+      {days.map(date => (
+        <Day 
+          key={date.format('DD/MM/YYYY')} 
+          date={date}
+          addBooking={(date) => {
+            setSelectedDate(date)
+            setModalOpen(true)
+          }} />
+      ))}
+
+      {modalOpen &&
+        <Modal>
+          <Button title="Close" onClick={() => setModalOpen(false)} />
+          <Typography margin="10px 0">{selectedDate.format('MMMM D : HH:mm')}</Typography>
+          <Button title="Book this slot" onClick={() => setModalOpen(false)} />
+        </Modal>
+      }
+    </React.Fragment>
+  )
+}
+
+CalendarDays.propTypes = {
+  dayOffset: PropTypes.number.isRequired
+}
+
+export default CalendarDays
